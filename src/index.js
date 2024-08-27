5fa79cd727cec652979a92a95bb7e7b8271015a0
Mahesh Mittha <maheshmittha93@gmail.com>
2024-08-27 23:19:41 +0530

    BookList Project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+function Greeting () {
+    return (
+        <>
+        <Person/>
+        <Message/>
+        </>
+    ) 
+}
+
+const Person = () => <h2>John Doe</h2>
+const Message = () => {
+ return <p>This is my message</p>
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Greeting/>) 
+
+
