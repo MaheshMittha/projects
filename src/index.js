69c40f73fe3341abaf945d7a508397aba27e7a9b
Mahesh Mittha <maheshmittha93@gmail.com>
2024-09-01 20:38:10 +0530

    props,kep,prop,object as prop, event basics,form submission

--- src/index.js
+++ src/index.js
@@ -1,42 +1,56 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 
-const BookList = ()=>{
-    return <section className="booklist">
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
+
+const author = "by Lisa Wingate"
+const title = "The Book of Lost Friends: A Novel"
+const img = './images/pexels-photo.jpeg'
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book author={author} title={title} img = {img} />
+      <Book author={author} title={title} img = {img} />
     </section>
-}
+  );
+};
 
-const Book = () => {
-return (
-    <article className="book" >
-    <Images/>
-    <Title/>
-    <Author/>
-</article>
-)
-}
+const Book = (props) => {
+    console.log(props);
+  return (
+    <article className="book">
+          <img className="img" 
+          src={props.img} alt= {props.title} />
+          <h2>{props.title}</h2>
+          <h4>{props.author}</h4>
+          {console.log(props)}
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
 
 
-const Images = ()=> <img src="https://m.media-amazon.com/images/I/816tHvSrjTL._AC_UL320_.jpg" alt="" srcset="" />
-const Title = ()=> <h2>The Book of Lost Friends: A Novel</h2>
-const Author = ()=> {
-    return <h2>by Lisa Wingate</h2>
-}  
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<BookList/>) 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // https://m.media-amazon.com/images/I/816tHvSrjTL._AC_UL320_.jpg
 // Title : The Book of Lost Friends: A Novel
 // Author : by Lisa Wingate
-
 
 // https://m.media-amazon.com/images/I/8196jTFIdLL._AC_UL320_.jpg
 // Title : The Paper Palace (Reese's Book Club): A Novel
@@ -46,3 +60,4 @@ root.render(<BookList/>)
 // Title : The Vanishing Half: A GMA Book Club Pick (A Novel)
 // Author : by Brit Bennett
 
+// 25th Ended
